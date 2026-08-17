ed32f0d866cf7f2c385a76429dd06f9809d25be1
Makizoe-git <k913590@gmail.com>
2026-08-17 10:55:03 +0700

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: './',
+  base: '/tabi-shiori/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
